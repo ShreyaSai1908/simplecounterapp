c1fa74f431b6727d365e177a0cdaa918826ab968
Editting <deltagare@LEXJONKAF33>
2021-08-03 18:53:31 +0200

    init

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+
+function Button ({ increment, onClickFunction }) 
+{
+    const handleClick = () => {
+      onClickFunction(increment)
+    }
+    return (
+      <button onClick={handleClick}>
+        +{increment}
+      </button>
+    )
+ }
+
+ export default Button;
+
+  
